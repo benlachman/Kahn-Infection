43803707460d4618bec6b89c2de25fbe18d1434d
Ben Lachman <ben@nicemohawk.com>
2014-10-07 13:51:46 -0400

    Initial Commit

---
+++ Kahn Infection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kahn Infection
+//
+//  Created by Ben Lachman on 10/7/14.
+//  Copyright (c) 2014 Ben Lachman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Kahn InfectionTests/Kahn_InfectionTests.swift
@@ -0,0 +1,36 @@
+//
+//  Kahn_InfectionTests.swift
+//  Kahn InfectionTests
+//
+//  Created by Ben Lachman on 10/7/14.
+//  Copyright (c) 2014 Ben Lachman. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Kahn_InfectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
